e83ce14154f91faf7c1f0d8bfd0e7daeed1da677
Benjamin Maynard <benmaynard@google.com>
2021-03-24 18:27:47 +0000

    Bump Kernel, OS and nfs-utils version

--- image/scripts/1_build_image.sh
+++ image/scripts/1_build_image.sh
@@ -92,7 +92,7 @@ build_install_nfs-utils() {
 
 }
 
-# download_kernel() downloads the 5.11 Kernel
+# download_kernel() downloads the 5.11.8 Kernel
 download_kernel() {
 
     # Make directory for kernel Images
@@ -103,16 +103,16 @@ download_kernel() {
     # Download Kernel .deb packages from kernel.ubuntu.com
     echo "Downloading kernel .deb files..."
     echo -e "------${SHELL_DEFAULT}"
-    curl -o ~/kernel-images/linux-headers-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
-    curl -o ~/kernel-images/linux-headers-5.11.0-051100_5.11.0-051100.202102142330_all.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-headers-5.11.0-051100_5.11.0-051100.202102142330_all.deb
-    curl -o ~/kernel-images/linux-image-unsigned-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-image-unsigned-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
-    curl -o ~/kernel-images/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11/amd64/linux-modules-5.11.0-051100-generic_5.11.0-051100.202102142330_amd64.deb
+    curl -o ~/kernel-images/linux-headers-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.8/amd64/linux-headers-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb
+    curl -o ~/kernel-images/linux-headers-5.11.8-051108_5.11.8-051108.202103200636_all.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.8/amd64/linux-headers-5.11.8-051108_5.11.8-051108.202103200636_all.deb
+    curl -o ~/kernel-images/linux-image-unsigned-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.8/amd64/linux-image-unsigned-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb
+    curl -o ~/kernel-images/linux-modules-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.8/amd64/linux-modules-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb
     echo -e -n "${SHELL_YELLOW}------"
     echo "DONE"
 
 }
 
-# install_kernel() installs the 5.9.1 kernel
+# install_kernel() installs the 5.11.8 kernel
 install_kernel() {
 
     # Install the new kernel using dpkg
