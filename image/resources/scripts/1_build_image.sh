00784b2a7a1cda9d6d4f4354a9e34e8dc6834830
Benjamin Maynard <benmaynard@google.com>
2022-02-18 15:57:29 +0000

    Update build script to install the latest hwe kernel
    Change-Id: I38141bb9a2199229919215bdd5af18f9ee875b0f

--- image/resources/scripts/1_build_image.sh
+++ image/resources/scripts/1_build_image.sh
@@ -164,33 +164,13 @@ install_netapp_exports() (
     echo "DONE"
 )
 
-# download_kernel() downloads the 5.11.8 Kernel
-download_kernel() {
-
-    # Make directory for kernel Images
-    echo -n "Creating directory for kernel Images... "
-    mkdir -p kernel-images
-    echo "DONE"
-
-    # Download Kernel .deb packages from kernel.ubuntu.com
-    echo "Downloading kernel .deb files..."
-    echo -e "------${SHELL_DEFAULT}"
-    curl -o kernel-images/linux-headers-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.8/amd64/linux-headers-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb
-    curl -o kernel-images/linux-headers-5.11.8-051108_5.11.8-051108.202103200636_all.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.8/amd64/linux-headers-5.11.8-051108_5.11.8-051108.202103200636_all.deb
-    curl -o kernel-images/linux-image-unsigned-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.8/amd64/linux-image-unsigned-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb
-    curl -o kernel-images/linux-modules-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.8/amd64/linux-modules-5.11.8-051108-generic_5.11.8-051108.202103200636_amd64.deb
-    echo -e -n "${SHELL_YELLOW}------"
-    echo "DONE"
-
-}
-
-# install_kernel() installs the 5.11.8 kernel
+# install_kernel() installs the latest hwe kernel
 install_kernel() {
 
-    # Install the new kernel using dpkg
+    # Install the new kernel
     echo "Installing kernel...."
     echo -e "------${SHELL_DEFAULT}"
-    dpkg -i kernel-images/*
+    apt-get install -y --install-recommends linux-generic-hwe-20.04
     echo -e -n "${SHELL_YELLOW}------"
     echo "DONE"
 
@@ -215,7 +195,6 @@ install_knfsd_agent
 install_knfsd_metrics_agent
 install_filter_exports
 install_netapp_exports
-download_kernel
 install_kernel
 copy_config
 
