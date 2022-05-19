b994cedfa6c3882b552fd6274b8bbff433cadd1e
Chris Chilvers <chris.chilvers@appsbroker.com>
2022-05-19 10:48:58 +0100

    Use LF line endings for bash scripts and Linux config files
    Create a .gitattributes file to set the line endings to LF for bash
    scripts and Linux config files.
    
    When checking out on other operating systems such as Windows the line
    endings may be changed to CRLF. This will cause errors for bash scripts
    and config files that require LF line endings.
    
    Change-Id: Ib2e9e293dde332f13c95816a44200df4b6af165c

--- deployment/terraform-module-knfsd/compute.tf
+++ deployment/terraform-module-knfsd/compute.tf
@@ -87,7 +87,7 @@ resource "google_compute_instance_template" "nfsproxy-template" {
     EXPORT_OPTIONS = var.EXPORT_OPTIONS
 
     # system
-    NFS_KERNEL_SERVER_CONF = file("${path.module}/resources/nfs-kernel-server-conf")
+    NFS_KERNEL_SERVER_CONF = file("${path.module}/resources/nfs-kernel-server.conf")
     NUM_NFS_THREADS        = var.NUM_NFS_THREADS
     VFS_CACHE_PRESSURE     = var.VFS_CACHE_PRESSURE
     DISABLED_NFS_VERSIONS  = var.DISABLED_NFS_VERSIONS
