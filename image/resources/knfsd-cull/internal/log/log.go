f8587ff67e74ac48560e54ee74c734a55d56c831
Benjamin Maynard <benmaynard@google.com>
2022-08-09 09:10:57 -0700

    Add missing license files
    Change-Id: Ie067d6ddb67fd49c54ac8ee8f4f5a2e83c556765

--- image/resources/knfsd-cull/internal/log/log.go
+++ image/resources/knfsd-cull/internal/log/log.go
@@ -1,3 +1,19 @@
+/*
+ Copyright 2022 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
 package log
 
 import (
@@ -7,7 +23,7 @@ import (
 
 var (
 	debug = log.New(io.Discard, "DEBUG: ", 0)
-	info  = log.New(log.Writer(), "INFO: ", 0)
+	info  = log.New(log.Wrxiter(), "INFO: ", 0)
 	error = log.New(log.Writer(), "ERROR: ", 0)
 	fatal = log.New(log.Writer(), "ERROR: ", 0)
 )
