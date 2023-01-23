4890808b509b5faeb8f7db8302d25c71e2df4331
Chris Chilvers <chris.chilvers@appsbroker.com>
2023-01-23 18:20:55 +0000

    Support consistent FSID numbers across a proxy cluster
    Build and deploy the new knfsd-fsidd service to support providing
    consistent FSID numbers across all proxy instances in a cluster.
    
    Change-Id: I5da364f073eac6893329ffe183224d04aa06e97e

--- deployment/terraform-module-knfsd/main.tf
+++ deployment/terraform-module-knfsd/main.tf
@@ -27,3 +27,16 @@ locals {
   )
   MIG_REPLACEMENT_METHOD_DEFAULT = var.ASSIGN_STATIC_IPS ? "RECREATE" : "SUBSTITUTE"
 }
+
+module "fsid_database" {
+  source                = "../database"
+  count                 = var.FSID_MODE == "external" && var.FSID_DATABASE_DEPLOY ? 1 : 0
+  project               = var.PROJECT
+  region                = var.REGION
+  zone                  = var.ZONE
+  name_prefix           = "${var.PROXY_BASENAME}-fsids"
+  proxy_service_account = var.SERVICE_ACCOUNT
+
+  # Simplify creating and destroying proxy cluster instances.
+  deletion_protection = false
+}
