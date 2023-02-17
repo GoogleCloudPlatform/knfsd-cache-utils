0c7346bac3a294c1b3745f9df4dc431f0a445738
Chris Chilvers <chris.chilvers@appsbroker.com>
2023-02-17 11:48:07 +0000

    Cache fsid/path lookups
    The fsid and path mapping cannot change once it has been assigned.
    Cache the fsid or path once it has been queried from the external
    database to avoid needing to make a network call out to the external
    database in the future.
    
    Change-Id: I5c7b3af639d5772fad922f944df32025c9779f44

--- image/resources/knfsd-fsidd/config.go
+++ image/resources/knfsd-fsidd/config.go
@@ -39,6 +39,7 @@ type Config struct {
 	SocketPath string         `ini:"socket"`
 	Database   DatabaseConfig `ini:"database"`
 	Debug      bool           `ini:"debug"`
+	Cache      bool           `ini:"cache"`
 }
 
 type DatabaseConfig struct {
@@ -101,6 +102,7 @@ func readEnv(cfg *Config) error {
 	err = multierr.Append(err, envBool(&cfg.Database.IAMAuth, "FSID_IAM_AUTH"))
 	err = multierr.Append(err, envBool(&cfg.Database.PrivateIP, "FSID_PRIVATE_IP"))
 	err = multierr.Append(err, envBool(&cfg.Debug, "FSID_DEBUG"))
+	err = multierr.Append(err, envBool(&cfg.Debug, "FSID_CACHE"))
 	return err
 }
 
